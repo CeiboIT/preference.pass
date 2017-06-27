5f34918ffa83dc85eb05b024cf26ba1088bef718
Epotignano <epotignano@gmail.com>
2017-06-27 16:07:38 -0500

    Fix

--- src/app/utils/store.ts
+++ src/app/utils/store.ts
@@ -1,5 +1,6 @@
 import * as _ from 'lodash';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/operator/let';
 
 export function onStateChange(store, path, cb) {
   store.let( store => store.select(state => _.get(state, path))).subscribe(cb);
@@ -7,10 +8,9 @@ export function onStateChange(store, path, cb) {
 
 export function onStateChangeObservable(store, path) {
   return Observable.create((observer) => {
-    store.let( store => store.select(state => _.get(state, path))
+    store.select(state => _.get(state, path))
       .subscribe((result) => {
         observer.next(result);
-      })
-    );
-});
+      });
+  });
 }

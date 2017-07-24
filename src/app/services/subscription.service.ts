23817b94ed4d7dc7c360b10a82b3a66e9a66c85a
leojquinteros <leojquinteros@gmail.com>
2017-07-24 17:52:13 -0300

    Servicio de subscriptions - 2

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers , RequestOptions } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Observable } from "rxjs/Rx";
 
 const server = 'http://localhost:3000/';
@@ -7,20 +7,14 @@ const uri = 'subscription/new';
 
 @Injectable()
 export class SubscriptionService {
-  
   private endpoint = server + uri;
   
   constructor(private http: Http) { }
 
   sendSubscription(body) {
-    debugger
-
     return new Promise((resolve, reject) => {
       let subscription = JSON.stringify(body);
-      let headers = new Headers({ 'Content-Type': 'application/json' });
-      let options = new RequestOptions({ headers: headers });
-
-      this.http.post(this.endpoint, subscription, options) 
+      this.http.post(this.endpoint, subscription) 
       .map((response: Response) => response.json())
       .subscribe(
         data => { 

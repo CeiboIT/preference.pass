60c62b8c631712591d58ef8003c8c804297516ad
JLMosconi <jlmosconi@gmail.com>
2017-08-10 16:36:29 -0300

    add user profile component

---
+++ src/app/components/user/user-profile/user-profile.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserProfileComponent } from './user-profile.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    
+  ],
+  declarations: [
+    UserProfileComponent
+  ],
+  exports: [
+    UserProfileComponent
+  ]
+})
+export class UserProfileModule { }
--- src/app/modules/user/user-profile-container/user-profile-container.module.ts
+++ src/app/modules/user/user-profile-container/user-profile-container.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserProfileContainerComponent } from './user-profile-container.component';
 import { MdButtonModule, MdCardModule } from '@angular/material';
+import { UserProfileContainerComponent } from './user-profile-container.component';
+import { UserProfileModule } from '../../../components/user/user-profile/user-profile.module';
 
 @NgModule({
   imports: [
     CommonModule,
     MdButtonModule,
-    MdCardModule
+    MdCardModule,
+    UserProfileModule
   ],
   exports: [
     UserProfileContainerComponent

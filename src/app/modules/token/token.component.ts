a72c4a96b04adc02167c542e64336b585f8936da
JLMosconi <jlmosconi@gmail.com>
2017-08-15 14:46:37 -0300

    avances token page

--- src/app/modules/token/token.component.ts
+++ src/app/modules/token/token.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import * as userActions from '../../actions/user';
 import { Store } from '@ngrx/store';
+import { trigger,style,transition,animate,query,stagger } from '@angular/animations';
 declare const LocalStorage: any;
 // import { lastVisitedUrl } from '../../constants/app.constants';
 
@@ -10,9 +11,27 @@ declare const LocalStorage: any;
   selector: 'app-token',
   template: `    
     <div class="container">
-      <h3>Welcome to PREFERENCE PASS</h3>
+      <div class="text-center my-5 py-5" @fadeUpAnimation>
+        <h1 class="fadeUp">Welcome to PREFERENCE PASS</h1>
+      </div>
     </div>
-  `
+  `,
+  animations: [
+    trigger('fadeUpAnimation', [
+      transition('* => *', [
+        query('.fadeUp', style({ opacity: 0, transform: 'translateY(10px)' })),
+
+        query('.fadeUp', stagger('100ms', [
+          animate('800ms .5s ease-in', style({ opacity: 1, transform: 'translateY(0)' })),
+        ])),
+
+        query('.fadeUp', [
+          animate(1000, style('*'))
+        ])
+        
+      ])
+    ])
+  ]
 })
 export class TokenComponent {
 

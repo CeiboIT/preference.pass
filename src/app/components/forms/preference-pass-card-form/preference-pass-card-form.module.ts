94adae48600c025b59c2f48cfdbc7440c2d2112d
Epotignano <epotignano@gmail.com>
2017-09-07 15:42:32 -0500

    Finish pp card screen

--- src/app/components/forms/preference-pass-card-form/preference-pass-card-form.module.ts
+++ src/app/components/forms/preference-pass-card-form/preference-pass-card-form.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { PreferencePassCardFormComponent } from './preference-pass-card-form.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {FieldsModule} from '../fields/fields.module';
+import {MdButtonModule} from "@angular/material";
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
-    FieldsModule
+    FieldsModule,
+    MdButtonModule
   ],
   declarations: [PreferencePassCardFormComponent],
   exports: [PreferencePassCardFormComponent]
--- src/app/modules/user/pp-card-container/pp-card-container.module.ts
+++ src/app/modules/user/pp-card-container/pp-card-container.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PpCardContainerComponent } from './pp-card-container.component';
 import {PreferencePassCardFormModule} from '../../../components/forms/preference-pass-card-form/preference-pass-card-form.module';
-import {PpCardDisplayModule} from "../../../components/widgets/pp-card-display/pp-card-display.module";
+import {PpCardDisplayModule} from '../../../components/widgets/pp-card-display/pp-card-display.module';
+import {MdButtonModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     PreferencePassCardFormModule,
-    PpCardDisplayModule
+    PpCardDisplayModule,
+    MdButtonModule
   ],
   declarations: [PpCardContainerComponent],
   exports: [PpCardContainerComponent]

474f2bdd771a13f45d019989236399f69a4c267a
JLMosconi <jlmosconi@gmail.com>
2017-09-07 16:28:22 -0300

    add loaders in grid

--- src/app/components/activities/activity-grid/activity-grid.component.ts
+++ src/app/components/activities/activity-grid/activity-grid.component.ts
@@ -10,6 +10,13 @@ import { Component, OnInit, Input } from '@angular/core';
 				<app-hot-deal-card [hotDeal]="activity" *ngIf="isHotDeal"></app-hot-deal-card>
 			</div>
 		</div>
+
+		<div class="row" *ngIf="!activities.length">
+			<div *ngFor="let item of items" class="mb-4 pb-2" [ngClass]="{'col-md-4 col-sm-6': !isHotDeal, 'col-md-3 col-sm-4 col-6': isHotDeal }">
+				<activity-card-loader *ngIf="!isHotDeal"></activity-card-loader>
+				<hot-deals-card-loader *ngIf="isHotDeal"></hot-deals-card-loader>
+			</div>
+		</div>
 	`,
 	styleUrls: ['./activity-grid.component.scss']
 })
@@ -17,9 +24,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ActivityGridComponent implements OnInit {
 	@Input() activities;
 	@Input() isHotDeal;
+	public items;
 	constructor() { }
 
-	ngOnInit() { }
+	ngOnInit() { 
+		const num = this.isHotDeal ? 4 : 3;
+		this.items = Array(num).fill(0).map((x,i)=>i);
+	}
 
 
 }
--- src/app/components/activities/activity-grid/activity-grid.module.ts
+++ src/app/components/activities/activity-grid/activity-grid.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ActivityGridComponent } from "./activity-grid.component";
 import { ActivityCardModule } from "../../widgets/activity-card/activity-card.module";
 import { HotDealCardModule } from "../../widgets/hot-deal-card/hot-deal-card.module";
+import { ActivityCardLoaderModule } from "../../loaders/activity-card-loader/activity-list.module";
+import { HotDealsCardLoaderModule } from "../../loaders/hot-deals-card-loader/hot-deals-card-loader.module";
 
 @NgModule({
     imports: [ 
         CommonModule,
         ActivityCardModule,
-        HotDealCardModule
+        HotDealCardModule,
+        ActivityCardLoaderModule,
+        HotDealsCardLoaderModule
     ],
     declarations: [ ActivityGridComponent ],
     exports: [ ActivityGridComponent ],

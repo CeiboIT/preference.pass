7805c3dfcdbc3c03bc904b9a485d3c2449a6f95e
Epotignano <epotignano@gmail.com>
2017-09-08 15:32:24 -0500

    Ports para api

--- src/app/constants/api.ts
+++ src/app/constants/api.ts
@@ -1,5 +1,5 @@
 //LOCAL
-export const subscriptionsEndpoint = 'http://localhost:3000/subscription';
+export const subscriptionsEndpoint = 'http://localhost:3030/subscription';
 
 //DEV
 //export const subscriptionsEndpoint = 'https://payments-preferencepass-symrfcsozd.now.sh/subscription';
@@ -8,5 +8,5 @@ export const subscriptionsEndpoint = 'http://localhost:3000/subscription';
 //export const subscriptionsEndpoint = 'https://payments.preferencepass.xyz/subscription';
 
 // misc
-export const cardsEndpoint = 'http://localhost:3030';
+export const cardsEndpoint = 'http://localhost:3010';
 export const codesEndpoint = 'http://localhost:3020';
